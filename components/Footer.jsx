406aef8d52b21f809d6c7cdd7417d9086d474184
mahbub045 <mahbubrmn45@gmail.com>
2024-01-08 00:40:35 +0600

    Design Home page and Footer Page

---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="text-center bg-red-200 text-black p-2">
+      <p className="text-sm">© 2024 All Rights Reserved by Food Valley</p>
+    </div>
+  );
+};
+
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,64 @@
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 
 const Home = () => {
   return (
     <>
       <Navbar />
+      <div className="min-h-screen ">
+        <div className="text-center">
+          <h2 className="uppercase font-semibold text-red-500 text-3xl p-1">
+            Chicken Crisper® Combos
+          </h2>
+          <div className="p-1">
+            <ul className="flex justify-center items-center font-semibold text-xs">
+              <li className="text-red-500">Menu</li>
+              <li>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="icon icon-tabler icon-tabler-chevron-right inline-block"
+                  width="16"
+                  height="16"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M9 6l6 6l-6 6" />
+                </svg>
+                Chicken Crisper® Combos
+              </li>
+            </ul>
+          </div>
+          <p className="p-1 font-semibold">
+            Find everything from our Big Mouth Burgers®, our always sizzling,
+            Full-on Fajitas and our famous Chicken Crispers®
+          </p>
+        </div>
+        <div className="flex justify-end mr-56 my-6">
+          <button className="font-semibold border rounded-md p-2">
+            Menu
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-4 h-4 inline-block"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m19.5 8.25-7.5 7.5-7.5-7.5"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+      <Footer />
     </>
   );
 };
